75e3d9e515a571557d8a5923e52c70262a169a05
John Truong <itsmehelp02@gmail.com>
2022-12-20 17:07:31 -0800

    secondSess

--- frc session 1/src/main/java/frc/robot/subsystems/Drivebase.java
+++ frc session 1/src/main/java/frc/robot/subsystems/Drivebase.java
@@ -8,17 +8,38 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import com.revrobotics.SparkMaxAlternateEncoder;
+import com.revrobotics.SparkMaxAlternateEncoder.Type;
 
 public class Drivebase extends SubsystemBase {
   /** Creates a new ExampleSubsystem. */
   public Drivebase() {}
 
+  private final int diameter = 10;
+
   CANSparkMax m_leftMotor = new CANSparkMax(0,MotorType.kBrushless);
   CANSparkMax m_rightMotor = new CANSparkMax(1,MotorType.kBrushless);
+
   DifferentialDrive m_drive = new DifferentialDrive(m_leftMotor, m_rightMotor);
+
+  SparkMaxAlternateEncoder m_leftEncoder = m.leftMotor.getAlternateEncoder(SparkMaxAlternateEncoder.Type.kQuadrature,20);
+  SparkMaxAlternateEncoder m_rightEncoder = m.rightMotor.getAlternateEncoder(SparkMaxAlternateEncoder.Type.kQuadrature,20);
 
   public void arcadeDrive(double forwardSpeed, double rotationSpeed){
     m_drive.arcadeDrive(forwardSpeed, rotationSpeed);
+  }
+
+  public void resetEncoders(){
+    m_leftEncoder.setPosition(0);
+    m_rightEncoder.setPosition(0);
+  }
+
+  public double getLeftDistanceInch(){
+    return (m_leftEncoder.getPosition() * diameter * Math.PI);
+  }
+
+  public void getRightDistanceInch(){
+    return (m_leftEncoder.getPosition() * diameter * Math.PI);
   }
 
   @Override
